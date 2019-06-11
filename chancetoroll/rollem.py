77049a433b978eb8edf0f655a1ff5251baaee364
asyks <aaron@aaronsykes.com>
2019-06-10 22:53:17 -0700

    Revise custom types, and add docstrings

--- chancetoroll/rollem.py
+++ chancetoroll/rollem.py
@@ -4,21 +4,31 @@ from typing import Tuple
 
 
 NumSet = Tuple[int]
-Combinations = Tuple[Tuple[int, ...]]
+Combinations = Tuple[Tuple[int]]
 
 
 def successful(S: NumSet, target: int, outcome: tuple) -> bool:
-    """ If any roll >= target return True """
+    """
+    If any roll >= target return True
+
+    param: S, the set of dice sides being tested, e.g. d6 = (1,2,3,4,5,6)
+    param: target, the threshold for successful outcomes
+    param: outcome, the selection to test for success
+    """
     return any([j in outcome for j in S[S.index(target):]])
 
 
 def calc_success_probability(size: int, k: int, target: int) -> float:
     """
-    Calculate the probability of rolling >= <target> on <numdice> of type <size>
+    Calculate the probability of rolling >= <target> on <k> of type <size>
 
-    First, determine the number of possible outcomes given an iterable of sides, and
-    the number of dice. Next, determine the number of success outcomes given an
-    iterable of sides, the number of dice, and the target value.
+    Determine the number of total possible outcomes, and successful outcomes given an
+    iterable of sides, and the number of dice. Then, calculate the success probability
+    as successful outcomes/total outcomes.
+
+    param: size, the number of sides of the dice being tested
+    param: k, the number of dice being tested
+    param: target, the threshold for successful outcomes
     """
     S: NumSet = tuple(range(1, size + 1))
 
@@ -29,7 +39,4 @@ def calc_success_probability(size: int, k: int, target: int) -> float:
         [True for outcome in total_combinations if successful(S, target, outcome)]
     ))
 
-    # Prob. of Success = Successful Combs. / Total Combs.
-    probability: float = success_combinations / total
-
-    return probability
+    return success_combinations / total
